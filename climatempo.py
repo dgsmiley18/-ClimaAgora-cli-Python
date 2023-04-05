ab3ba7c8e7d57147711c328ba2820f4f87937b24
Douglas Vianna <douglasvianna230@gmail.com>
2023-04-05 17:26:56 -0300

    feat: :zap: Adicionado o requests para melhor desempenho
    Foi removido o selenium por conta de não ser necessario para um scraping simples, agora está sendo usado o bs4 e requests aumentando em muito a velocidade

--- climatempo.py
+++ climatempo.py
@@ -1,23 +1,21 @@
 import time
+from bs4 import BeautifulSoup
+import requests
 import argparse
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-# Instancia o driver do chrome e desabilita os logs
-chrome_options = Options()
-chrome_options.add_argument("headless")
-chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(options=chrome_options)
 
 # Instancia o ArgumentParser
 parser = argparse.ArgumentParser(prog='ClimaTempo',description="Veja o clima de hoje direto do seu terminal!")
 
-# Adiciona o argumento "--local" com explicação
-parser.add_argument('-l', '--local', help='especifique um local', required=True)
+#Adiciona o argumento "--local" com explicação
+parser.add_argument('-l', '--local', help='especifique um local', default='+')
 args = parser.parse_args()
 
 # Entra no site do google e procura todas as informações necessarias
-driver.get("https://www.google.com/search?q=clima+agora+"+args.local.replace(" ","+"))
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+html = requests.get(f"https://www.google.com/search?q=clima+agora+{args.local}", headers=headers).content
+soup = BeautifulSoup(html, 'html.parser')
+body = soup.find("body")
+'''
 cidadenome = driver.find_element('xpath','//*[@id="oFNiHe"]/div/div/div/div[1]/span[2]').text
 tempmin = driver.find_element('xpath','//*[@id="wob_dp"]/div[1]/div[3]/div[2]/span[1]').text
 tempmax = driver.find_element('xpath','//*[@id="wob_dp"]/div[1]/div[3]/div[1]/span[1]').text
@@ -27,14 +25,22 @@ umidade = driver.find_element('xpath','//*[@id="wob_hm"]').text
 temperatura = driver.find_element('xpath','//*[@id="wob_tm"]').text
 infohoje = driver.find_element('xpath','//*[@id="wob_dc"]').text
 tempo = driver.find_element('xpath','//*[@id="wob_dts"]').text
+'''
 time.sleep(1)
 
+cidadenome = soup.find("span", class_="BBwThe").text
+tempmin = soup.select_one("#wob_dp > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > span:nth-child(1)").text
+tempmax = soup.select_one("#wob_dp > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > span:nth-child(1)").text
+temperatura = soup.select_one("#wob_tm").text
+chuva = soup.select_one("#wob_pp").text
+vento = soup.select_one("#wob_ws").text
+umidade = soup.select_one("#wob_hm").text
+infohoje = soup.select_one("#wob_dc").text
+tempo = soup.select_one("#wob_dts").text.capitalize()
 print(f"Temperatura: {temperatura}°C   🏙 Cidade: {cidadenome}")
 print(f"\u2B06 Minima: {tempmin}° \u2B07 Maxima: {tempmax}°")
 print("\u2614 Chuva: " + chuva)
 print("🍃 Vento: " + vento)
 print("Umidade: " + umidade)
-print("Situação: " + infohoje)
+print("Situação Hoje: " + infohoje)
 print(tempo)
-# Fecha o driver para evitar de ficar aberto
-driver.close()
