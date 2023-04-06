4b53b1634b2aac0f0d38bac4d15b52e1c1b637fc
Douglas Vianna <douglasvianna230@gmail.com>
2023-04-06 10:31:12 -0300

    style: :pencil2: Trocado nomes de váriaveis
    Troquei o nome de váriaveis para fácil entendimento e documentei o que precisava.

--- climatempo.py
+++ climatempo.py
@@ -18,19 +18,20 @@ body = soup.find("body")
 
 time.sleep(1)
 
-cidadenome = soup.find("span", class_="BBwThe").text
-tempmin = soup.select_one("#wob_dp > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > span:nth-child(1)").text
-tempmax = soup.select_one("#wob_dp > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > span:nth-child(1)").text
+# Encontra as informações necessarias e mostra na tela
+cidade_nome = soup.find("span", class_="BBwThe").text
+temp_min = soup.select_one("#wob_dp > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > span:nth-child(1)").text
+temp_max = soup.select_one("#wob_dp > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > span:nth-child(1)").text
 temperatura = soup.select_one("#wob_tm").text
 chuva = soup.select_one("#wob_pp").text
 vento = soup.select_one("#wob_ws").text
 umidade = soup.select_one("#wob_hm").text
-infohoje = soup.select_one("#wob_dc").text
+info_hoje = soup.select_one("#wob_dc").text
 tempo = soup.select_one("#wob_dts").text.capitalize()
-print(f"Temperatura: {temperatura}°C   🏙 Cidade: {cidadenome}")
-print(f"\u2B06 Minima: {tempmin}° \u2B07 Maxima: {tempmax}°")
+print(f"Temperatura: {temperatura}°C   🏙 Cidade: {cidade_nome}")
+print(f"\u2B06 Minima: {temp_min}° \u2B07 Maxima: {temp_max}°")
 print("\u2614 Chuva: " + chuva)
 print("🍃 Vento: " + vento)
 print("Umidade: " + umidade)
-print("Situação Hoje: " + infohoje)
+print("Situação Hoje: " + info_hoje)
 print(tempo)
